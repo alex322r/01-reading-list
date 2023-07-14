8e7fda6d16a232e982b90eb764f09390cd97180b
alexis <alexis@alexis>
2023-07-13 23:28:50 -0500

    test added

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,7 +3,7 @@ import { useGlobalContext } from "../context/globalContext"
 export default function Book({ title, cover, someAction }) {
 
     return (
-        < div id={title} key={title} onClick={() => someAction(title)} >
+        < div className="book" id={title} key={title} onClick={() => someAction(title)} >
             <img style={{ width: '200px' }} src={cover} />
         </div >
     )
